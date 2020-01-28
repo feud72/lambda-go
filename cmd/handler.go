5cc4093d49655039f4faf95df5ca479e49a3a277
feud72 <feud72@gmail.com>
2020-01-28 17:05:19 +0900

    Launch Wetube-go proto (under writing)

--- cmd/handler.go
+++ cmd/handler.go
@@ -6,22 +6,18 @@ import (
 	"os"
 
 	"github.com/apex/gateway"
-	"github.com/gin-gonic/gin"
+	v1 "github.com/feud72/Wetube-go/v1"
 )
 
-func test(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"success": true, "message": "test lambda API"})
-}
-
 func main() {
+
 	addr := ":3000"
 	mode := os.Getenv("GIN_MODE")
-	g := gin.New()
-	g.GET("/test", test)
 
+	app := v1.Engine()
 	if mode == "release" {
-		log.Fatal(gateway.ListenAndServe(addr, g))
+		log.Fatal(gateway.ListenAndServe(addr, app))
 	} else {
-		log.Fatal(http.ListenAndServe(addr, g))
+		log.Fatal(http.ListenAndServe(addr, app))
 	}
 }
